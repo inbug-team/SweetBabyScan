c7d2f675c7e554904922573054be557fc4b122d6
inbug-team <admin@inbug.org>
2022-05-20 21:10:56 +0800

    [+]使用说明 [+]linux编译脚本

---
+++ build.sh
@@ -0,0 +1,4 @@
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -trimpath -o SbScanLinuxAmd64
+GOOS=linux GOARCH=386 go build -ldflags="-s -w" -trimpath -o SbScanLinux386
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w -H windowsgui" -trimpath -o SbScanAmd64.exe
+GOOS=windows GOARCH=386 go build -ldflags="-s -w -H windowsgui" -trimpath -o SbScan386.exe
