bc81c78870103af07509bdee7a8fc1d6e0c6a37e
inbug-team <admin@inbug.org>
2022-05-28 16:02:26 +0800

    [+]upx

--- build_mac.sh
+++ build_mac.sh
@@ -1,2 +1,5 @@
 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -trimpath -o SbScanMacAmd64
 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -trimpath -o SbScanMacArm64
+
+upx -9 SbScanMacAmd64
+upx -9 SbScanMacArm64
