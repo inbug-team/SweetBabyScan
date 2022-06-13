fc42fa9ecaf0e5f22e90c318a55c7d5d00d18485
inbug-team <admin@inbug.org>
2022-06-13 09:41:44 +0800

    [+]linux arm编译

--- build_mac.sh
+++ build_mac.sh
@@ -1,4 +1,5 @@
 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -trimpath -o SbScanMacAmd64
 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -trimpath -o SbScanMacArm64
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -trimpath -o SbScanLinuxArm64
 
 upx -9 SbScanMacAmd64
